c053fff13b7eefad40ce958039e3858614c69aef
Jaydeep Singh <jdsingh94@gmail.com>
2017-12-05 12:26:31 -0800

    hello world

---
+++ cis22c_finalproj_js_cars/main.cpp
@@ -0,0 +1,18 @@
+/*
+main.cpp
+Jaydeep Singh
+Final Project
+Car Inventory
+CIS 22C - Goel
+*/
+
+#include <iostream>
+#include <string>
+
+int main()
+{
+	std::cout << "Hello World!" << std::endl;
+
+	system("pause");
+	return 0;
+}
