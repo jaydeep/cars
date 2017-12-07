279c47e6ff5a7aabaaf88c1dc6feec69be44d7be
Jaydeep Singh <jdsingh94@gmail.com>
2017-12-06 23:18:14 -0800

    addRecord adds record to tree and bst

--- cis22c_finalproj_js_cars/headnode.h
+++ cis22c_finalproj_js_cars/headnode.h
@@ -10,6 +10,12 @@ private:
 	int arySize;
 	MyHash pHash;
 	BinarySearchTree<std::string> *pTree;
+	void addRecord(MyCar*);
+	void removeRecord();
+	void search(std::string);
+	void printTree();
+	void printTable();
+	void printSortedTable();
 
 public:
 	headNode(int, int, MyHash, BinarySearchTree<std::string>*);
@@ -21,5 +27,36 @@ headNode::headNode(int c, int s, MyHash p, BinarySearchTree<std::string>* t)
 	arySize = s;
 	pHash = p;
 	pTree = t;
+}
+
+void headNode::addRecord(MyCar* carToAdd)
+{
+	pHash.add(carToAdd);
+	pTree->add(pTree->getRoot(), carToAdd->getVIN());
+}
+
+void headNode::removeRecord()
+{
+
+}
+
+void headNode::search(std::string searchKey)
+{
+
+}
+
+void headNode::printTree()
+{
+
+}
+
+void headNode::printTable()
+{
+
+}
+
+void headNode::printSortedTable()
+{
+
 }
 #endif
--- cis22c_finalproj_js_cars/main.cpp
+++ cis22c_finalproj_js_cars/main.cpp
@@ -99,4 +99,3 @@ int main() {
 	system("pause");
 	return 0;
 }
-
